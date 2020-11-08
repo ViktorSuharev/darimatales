8311c9c2bb3389dc2c20a398fd91fabd3445aeb7
ViktorSuharev <viktor.suhareff@yandex.ru>
2020-11-08 23:29:43 +0300

    uncommit inner svg

--- src/app/tales/twenty-fifth/twenty-fifth.component.ts
+++ src/app/tales/twenty-fifth/twenty-fifth.component.ts
@@ -29,22 +29,22 @@ export class TwentyFifthComponent {
       '    </svg>\n' +
       '  </div>';
 
-    // this.svgInnerImage = '' +
-    //   '<div style="width: 100%; height: 100%">\n' +
-    //   '  <svg style="width: 100%; height: 100%">\n' +
-    //   '  <defs>\n' +
-    //   '    <linearGradient id="three_opacity_stops">\n' +
-    //   '      <stop offset="0%" style="stop-color: #906; stop-opacity: 1.0"/>\n' +
-    //   '      <stop offset="50%" style="stop-color: #906; stop-opacity: 0.3"/>\n' +
-    //   '      <stop offset="100%" style="stop-color: #906; stop-opacity: 0.10"/>\n' +
-    //   '    </linearGradient>\n' +
-    //   '  </defs>' +
-    //   '    <rect x="' + startPointX * Math.sqrt(1.4) + '" y="' + startPointY * Math.sqrt(2) +
-    //          '" height="' + squareLength / Math.sqrt(1.5) + '" width="' + squareLength / Math.sqrt(1.5) + '"\n' +
-    //   '         style="fill: url(#three_opacity_stops);"\n' +
-    //   '         transform="rotate(45 ' + rotationPointX + ' ' + rotationPointY + ')"\n' +
-    //   '    ></rect>\n' +
-    //   '  </svg>\n' +
-    //   '</div>';
+    this.svgInnerImage = '' +
+      '<div style="width: 100%; height: 100%">\n' +
+      '  <svg style="width: 100%; height: 100%">\n' +
+      '  <defs>\n' +
+      '    <linearGradient id="three_opacity_stops">\n' +
+      '      <stop offset="0%" style="stop-color: #906; stop-opacity: 1.0"/>\n' +
+      '      <stop offset="50%" style="stop-color: #906; stop-opacity: 0.3"/>\n' +
+      '      <stop offset="100%" style="stop-color: #906; stop-opacity: 0.10"/>\n' +
+      '    </linearGradient>\n' +
+      '  </defs>' +
+      '    <rect x="' + startPointX * Math.sqrt(1.4) + '" y="' + startPointY * Math.sqrt(2) +
+             '" height="' + squareLength / Math.sqrt(1.5) + '" width="' + squareLength / Math.sqrt(1.5) + '"\n' +
+      '         style="fill: url(#three_opacity_stops);"\n' +
+      '         transform="rotate(45 ' + rotationPointX + ' ' + rotationPointY + ')"\n' +
+      '    ></rect>\n' +
+      '  </svg>\n' +
+      '</div>';
   }
 }
