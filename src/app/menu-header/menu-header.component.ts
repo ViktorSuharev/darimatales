c01ea28a06a34009ab320889df77a4b3aaccc621
v.suharev <viktor.suhareff@yandex.ru>
2022-12-25 00:21:15 +0600

    fix styles

--- src/app/menu-header/menu-header.component.ts
+++ src/app/menu-header/menu-header.component.ts
@@ -7,16 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu-header.component.css']
 })
 export class MenuHeaderComponent {
-  isDropDownOpened = false;
+  public isDropDownOpened = false;
 
-  constructor(private route: Router) {}
+  constructor(private readonly route: Router) {}
 
   flipDropDown(): void {
     this.isDropDownOpened = !this.isDropDownOpened;
   }
 
   selectTale(page: string): void {
-    this.route.navigate(['/' + page]);
+    this.route.navigateByUrl(page);
     this.flipDropDown();
   }
 }
