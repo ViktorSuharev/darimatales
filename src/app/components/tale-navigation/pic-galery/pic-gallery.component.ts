a81894a41ae5b365568c6d09363e19fba28c814f
v.suharev <viktor.suhareff@yandex.ru>
2023-01-07 00:50:51 +0600

    do not hardcode styles for pic gallery

--- src/app/components/tale-navigation/pic-galery/pic-gallery.component.ts
+++ src/app/components/tale-navigation/pic-galery/pic-gallery.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, HostBinding, HostListener, Input, OnInit, Output} from '@angular/core';
 import {Option} from '../../../model/option.model';
 
 @Component({
@@ -6,7 +6,7 @@ import {Option} from '../../../model/option.model';
   templateUrl: './pic-gallery.component.html',
   styleUrls: ['./pic-gallery.component.less']
 })
-export class PicGalleryComponent {
+export class PicGalleryComponent implements OnInit {
 
   @Input()
   options: Option[] = [];
@@ -14,8 +14,42 @@ export class PicGalleryComponent {
   @Output()
   select: EventEmitter<Option> = new EventEmitter<Option>();
 
+  topStyle: string = '';
+  leftStyle: string = '';
+  marginStyle: string = '';
+
+  private widthPx: number = 80;
+  private heightPx: number = 50;
+  private marginPx: number = 30;
+  private factor: number = 2;
+
+  ngOnInit() {
+    const topDiff: number = this.heightPx / 2;
+    const leftDiff: number = (this.options.length * this.widthPx + (this.options.length - 1) * this.marginPx) / 2;
+    this.topStyle = `calc(50% - ${PicGalleryComponent.px(topDiff)})`;
+    this.leftStyle = `calc(50% - ${PicGalleryComponent.px(leftDiff)}`;
+    this.marginStyle = PicGalleryComponent.px(this.marginPx);
+  }
+
+  @HostBinding('style.width') widthStyle: string = PicGalleryComponent.px(this.widthPx);
+  @HostBinding('style.height') heightStyle: string = PicGalleryComponent.px(this.heightPx);
+
+  @HostListener('mouseenter') onEnter() {
+    this.widthStyle = PicGalleryComponent.px(this.widthPx * this.factor);
+    this.heightStyle = PicGalleryComponent.px(this.heightPx * this.factor);
+  }
+
+  @HostListener('mouseleave') onLeave() {
+    this.widthStyle = PicGalleryComponent.px(this.widthPx);
+    this.heightStyle = PicGalleryComponent.px(this.heightPx);
+  }
+
   onClick(option: Option, e: MouseEvent): void {
     e.stopPropagation();
     this.select.emit(option);
+  }
+
+  private static px(value: number): string {
+    return `${value}px`;
   }
 }
