252241f794503920d07f1f37997ce3580b6dda00
v.suharev <viktor.suhareff@yandex.ru>
2023-01-18 23:13:04 +0600

    add rhombus

---
+++ src/app/tales/twenty-fifth/front-picture/rhombus.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rhombus',
+  templateUrl: './rhombus.component.html',
+  styleUrls: ['./rhombus.component.less']
+})
+export class RhombusComponent {
+
+}
